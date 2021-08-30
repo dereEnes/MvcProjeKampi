eec385eda49ba6b2afbf206d16bd9f14375c10de
dereEnes <dere.enes01@gmail.com>
2021-08-30 13:36:14 +0300

    heading,content added

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class ContentController : Controller
+    {
+        ContentManager _contentManager = new ContentManager(new EfContentDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContentByHeding(int id)
+        {
+            var contents =_contentManager.GetListById(id);
+            return View(contents);
+        }
+    }
+}
---
+++ Controllers/HeadingController.cs
@@ -0,0 +1,70 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        private HeadingManager _headingManager = new HeadingManager(new EfHeadingDal());
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryDal());
+        private WriterManager _writerManager = new WriterManager(new EfWriterDal());
+        private ContentManager _contentManager = new ContentManager(new EfContentDal());
+
+        public ActionResult Index()
+        {
+            var result = _headingManager.GetAll();
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult Insert(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            _headingManager.Insert(heading);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            List<SelectListItem> catetories = (from x in _categoryManager.GetAll()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.CategoryName,
+                                                   Value= x.CategoryId.ToString()
+                                               }).ToList();
+            List<SelectListItem> writers = (from x in _writerManager.GetAll()
+                                               select new SelectListItem
+                                               {
+                                                   Text = x.WriterName+" "+x.WriterSurName,
+                                                   Value = x.WriterId.ToString()
+                                               }).ToList();
+            ViewBag.valueCategories = catetories;
+            ViewBag.valueWriters = writers;
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            _headingManager.Delete(_headingManager.GetById(id));
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            Heading heading = _headingManager.GetById(id);
+            return View(heading);
+        }
+        [HttpPost]
+        public ActionResult Update(Heading heading)
+        {
+            _headingManager.Update(heading);
+            return RedirectToAction("Index");
+        }
+        
+
+    }
+}
